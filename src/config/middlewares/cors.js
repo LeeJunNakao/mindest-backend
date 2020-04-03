2bbd8e133ef62bd376e7838b411183991f12c0fe
Alex Lee Jun Nakao <leejun.nakao@gmail.com>
2020-04-03 19:36:12 -0300

    includio o endereço do servidor frontend no cors

--- src/config/middlewares/cors.js
+++ src/config/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = (req,res,next)=>{
-	res.header('Access-Control-Allow-Origin','*')
+	res.header('Access-Control-Allow-Origin','https://mindest.herokuapp.com')
 	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS')
 	res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,token')
 	
