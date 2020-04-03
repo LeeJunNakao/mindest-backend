863bb79e1e0f66b7e89dde9e64f8616e729def6a
Alex Lee Jun Nakao <leejun.nakao@gmail.com>
2020-04-03 19:13:18 -0300

    Adicionado rota get /register para teste

--- src/config/routes/openRoutes/routes.js
+++ src/config/routes/openRoutes/routes.js
@@ -3,6 +3,9 @@ const Year = require('../../../api/models/game/yearGame/yearGameService')
 module.exports = (app)=>{
     
     app.post('/register',register)
+    app.get('/register',(req,res,next)=>{
+        res.status(200).send({ response: ok })
+    })
     app.post('/login',login)
     app.post('/validateToken',validateToken)
     Year.register(app,'/year')
