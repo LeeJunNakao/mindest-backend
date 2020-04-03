34f96273fc96b389ce2c655c95ac3505b9cee290
Alex Lee Jun Nakao <leejun.nakao@gmail.com>
2020-04-03 17:01:09 -0300

    incluido script start no pakage.json

--- src/config/database/config.js
+++ src/config/database/config.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-module.exports = mongoose.connect('mongodb://localhost/mindest', {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true,});
+const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/mindest';
+
+module.exports = mongoose.connect(url, {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true,});
 
 mongoose.connection.once('open',()=>{
     console.log('mongoose conectado')
